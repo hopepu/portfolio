434de4d12df32894415bdb3ae5facebed1cd7a21
hopepu <whdydwo2@gmail.com>
2024-12-01 23:09:31 +0900

    v1.0.7 eslint bug fix 2

--- src/react-anchor-link-smooth-scroll.d.ts
+++ src/react-anchor-link-smooth-scroll.d.ts
@@ -1,4 +1,13 @@
 declare module 'react-anchor-link-smooth-scroll' {
-    const AnchorLink: React.ComponentType<never>;
+    import * as React from 'react';
+
+    interface AnchorLinkProps {
+        href: string;
+        offset?: number | string;
+        children: React.ReactNode;
+        className?: string; // className 속성을 추가합니다.
+    }
+
+    const AnchorLink: React.FC<AnchorLinkProps>;
     export default AnchorLink;
 }
